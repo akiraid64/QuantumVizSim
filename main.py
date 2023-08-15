df82ad828b1b47233fc99877116c71f29d70b8f3
Aditya Kumar Sabat <114684499+akiraid64@users.noreply.github.com>
2023-08-15 23:17:44 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui import QuantumApp
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = QuantumApp()
-    sys.exit(app.exec_())
